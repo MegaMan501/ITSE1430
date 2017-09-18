f3716395762d8931070446ff4075c31acd048bbc
Illuminati <Illuminati@DESKTOP-T3ON4JN>
2017-09-18 00:57:15 -0500

    Finished Lab 01

--- Labs/lab01/lab01/Program.cs
+++ Labs/lab01/lab01/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Name: Mohamed Rahaman
+ * Class: ITSE 1430 20630
+ * Date: September 09, 2017
+ * Lab: 01
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +14,29 @@ namespace lab01
 {
     class Program
     {
-        static char Input()
+        static string movieTitle = "";
+        static string movieDescription = "";
+        static int movieRuntime = 0;
+        static bool movieOwned = false;
+        
+        static char GetInput()
         {
             do
             {
-                System.Console.WriteLine("L)ist Movies\n" +
-                                         "A)dd Movies\n" +
-                                         "R)emove Movies\n" +
-                                         "Q)uit\nEnter your choice: ");
+                Console.WriteLine();
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("".PadLeft(15, '-'));
+                Console.WriteLine("L)ist Movies");
+                Console.WriteLine("A)dd Movies");
+                Console.WriteLine("R)emove Movies");
+                Console.WriteLine("Q)uit");
+                Console.WriteLine("".PadLeft(15, '-'));
+                Console.Write("Your Choice: ");
+
                 string input = Console.ReadLine().Trim();
 
                 if (input != null && input.Length != 0)
                 {
-                    //String comparison
-                    if (String.Compare(input, "A", true) != 0)
-                        return 'A';
-
-                    //char comparison
                     char letter = Char.ToUpper(input[0]);
                     if (letter == 'L')
                         return 'L';
@@ -35,37 +47,125 @@ namespace lab01
                     else if (letter == 'Q')
                         return 'Q';
                 }
-                //System.Console.Write("\nchoice: {0}", choice);
-                /*switch (c)
-                {
-                    case "L":
-                        //printMovies(); 
-                        break; 
-                    case "l":
-                        break;
-                    case "A":
-                        break;
-                    case "a":
-                        break; 
-                    case "R":
-                        break;
-                    case "r":
-                        break; 
-                    case "Q":
-                        break; 
-                    case "q":
-                        break; 
-                    default:
-                        break;
-                }*/
-            } while(true);
+
+                Console.WriteLine("Please choose a valid option.");
+            } while (true);
         }
         static void Main(string[] args)
         {
+            bool quit = false;
+            do
+            {
+                char choice = GetInput();
+                switch (choice)
+                {
+                    case 'l':
+                    case 'L': ListMovies(); break;
+                    case 'a':
+                    case 'A': AddMovies(); break;
+                    case 'r':
+                    case 'R': RemoveMovies(); break;
+                    case 'q':
+                    case 'Q': quit = true; break;                         
+                };
+
+            } while (!quit);
+        }
+
+        private static void RemoveMovies()
+        {
+            Console.WriteLine("".PadLeft(15, '-'));
+            Console.Write("Are you sure you want to delete the movie? (Y/N): ");
+            bool removeStatus = ReadYesNo();
+
+            if (removeStatus)
+            {
+                movieTitle = "";
+                movieDescription = "";
+                movieRuntime = 0;
+                movieOwned = false;
+            }
+            Console.WriteLine();
+        }
+
+        private static void AddMovies()
+        {
+            Console.WriteLine("".PadLeft(15, '-'));
+
+            do
+            {
+                Console.Write("Enter a title: ");
+                movieTitle = Console.ReadLine().Trim();
+
+                if (String.IsNullOrEmpty(movieTitle))
+                    Console.WriteLine("You must enter a value");
+
+            } while (String.IsNullOrEmpty(movieTitle));
+            
            
-           
-            System.Console.Write("\nPress any key to continue...");
-            System.Console.ReadLine();
+            Console.Write("Enter a description [Optional]: ");
+            movieDescription = Console.ReadLine();
+
+            do
+            {
+                Console.Write("Enter the runtime in minutes [Optional]: ");
+                movieRuntime = ReadInteger();
+
+                if (!(movieRuntime >= 0))
+                    Console.WriteLine("\tYou must enter a value >= 0");
+                
+            } while (!(movieRuntime >= 0));
+
+            Console.Write("Do you own this movie? (Y/N): ");
+            movieOwned = ReadYesNo();
+            Console.WriteLine();
+        }
+
+        private static void ListMovies()
+        {
+            Console.WriteLine("".PadLeft(15, '-'));
+
+            if (String.IsNullOrEmpty(movieTitle))
+                Console.WriteLine("NO MOVIES AVAILABLE");
+            else
+            {
+                string result = $"Movie Title = {movieTitle}\nDescription = {movieDescription}\n" +
+                                $"Run Time = {movieRuntime}\nStatus = {(movieOwned ? "Owned" : "Not Owned")}";
+                Console.WriteLine(result);
+            }
+            Console.WriteLine();
+        }
+
+        static int ReadInteger()
+        {
+            do
+            {
+                string input = Console.ReadLine();
+
+                // integer result
+                if (Int32.TryParse(input, out int result))
+                    return result;
+
+                Console.Write("Enter a valid integer: ");
+
+            } while (true);
+        }
+        static bool ReadYesNo()
+        {
+            do
+            {
+                string input = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(input))
+                {
+                    switch (Char.ToUpper(input[0]))
+                    {
+                        case 'Y': return true;
+                        case 'N': return false;
+                    }
+                    Console.Write("Enter either Y or N: ");
+                }
+            } while (true);
         }
     }
 }
