6f3736a6749c170c512fc2b24188b53a9f44b068
mohamed.rahaman_s <mohamed.rahaman_s@NEITD-222515>
2017-11-15 18:30:29 -0600

    Finished Section 4

--- ClassWork/Section4/Nile.Stores.SQL/SQLProductDatabase.cs
+++ ClassWork/Section4/Nile.Stores.SQL/SQLProductDatabase.cs
@@ -22,7 +22,21 @@ namespace Nile.Stores.Sql
 
         protected override Product AddCore( Product product )
         {
-            throw new NotImplementedException();
+            var id = 0; 
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("AddProduct", conn) 
+                { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = product.Name;
+                cmd.Parameters.AddWithValue("@description", product.Description);
+                cmd.Parameters.AddWithValue("@price", product.Price);
+                cmd.Parameters.AddWithValue("@isDiscontinued", product.IsDiscontinued);
+
+                id = Convert.ToInt32(cmd.ExecuteScalar());
+            };
+
+            return GetCore(id);
         }
 
         protected override IEnumerable<Product> GetAllCore()
@@ -63,17 +77,81 @@ namespace Nile.Stores.Sql
 
         protected override Product GetCore( int id )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("GetProduct", conn) { CommandType = CommandType.StoredProcedure};
+                cmd.Parameters.AddWithValue("@id", id);
+
+                // Using a dataset instead of a reader
+                var ds = new DataSet();
+                var da = new SqlDataAdapter() 
+                {
+                    SelectCommand = cmd,
+                    //DeleteCommand, 
+                    //UpdateCommand,
+                    //InsertCommand
+                };
+
+                da.Fill(ds);
+
+                var table = ds.Tables.OfType<DataTable>().FirstOrDefault(); 
+                if (table != null)
+                {
+                    var row = table.AsEnumerable().FirstOrDefault();
+                    if (row != null)
+                    {
+                        return new Product() {
+                            Id = Convert.ToInt32(row["Id"]),
+                            Name = row.Field<string>("Name"),
+                            Description = row.Field<string>("Description"),
+                            Price = row.Field<decimal>("Price"),
+                            IsDiscontinued = row.Field<bool>("IsDiscontinued")
+                        };
+                    };
+                };
+            };
+
+            return null;
         }
 
         protected override void RemoveCore( int id )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                // Hacking in with sql injection
+                //var input = "0'; DELETE FROM Products;SELECT '"; 
+                //var CommandText = $"DELETE FROM Products WHERE Id =  '{input}'";
+                //DELETE FROM Products WHERE Id =  '0'; 
+
+                // Alternative approach to creating a command
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "RemoveProduct";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // The long way
+                var parameter = new SqlParameter("@id", id);
+                cmd.Parameters.Add(parameter);
+
+                cmd.ExecuteNonQuery();
+            };
         }
 
         protected override Product UpdateCore( Product existing, Product newItem )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("UpdateProduct", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.AddWithValue("@id", existing.Id);
+                cmd.Parameters.AddWithValue("@name", newItem.Name);
+                cmd.Parameters.AddWithValue("@description", newItem.Description);
+                cmd.Parameters.AddWithValue("@price", newItem.Price);
+                cmd.Parameters.AddWithValue("@isDiscontinued", newItem.IsDiscontinued);
+
+                cmd.ExecuteNonQuery();
+            };
+
+            return GetCore(existing.Id);
         }
 
         private SqlConnection OpenDatabase()
